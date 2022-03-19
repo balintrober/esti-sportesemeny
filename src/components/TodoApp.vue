6eb96adb7c3cc1bfabfcd863fef6310504ae6b43
Róbert Bálint <balint.robert78@gmail.com>
2022-03-19 11:13:09 +0100

    POST megvalósítása fetch és axios segítségével

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -3,7 +3,13 @@
   <div>
     <form @submit.prevent="kuld">
       <div>
-        <input type="text" name="nev" v-model="nev" placeholder="Teszt Elek" @change="check"/>
+        <input
+          type="text"
+          name="nev"
+          v-model="nev"
+          placeholder="Teszt Elek"
+          @change="check"
+        />
       </div>
       <div>
         <input type="date" name="szul" v-model="szul" />
@@ -36,33 +42,55 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
       nevezes: null,
-      // {
-      //   nev: "",
-      //   szul: "",
-      //   email: "",
-      //   nem: "",
-      // },
-      nev: '',
-      szul: '',
-      email: '',
-      nem: '',
-      aszf: '',
-      kitoltve: true
+      nev: "",
+      szul: "",
+      email: "",
+      nem: "",
+      aszf: "",
+      kitoltve: true,
     };
   },
   methods: {
     kuld() {
-      this.nevezes.nev = this.nev;
+      this.nevezes = {
+        nev: this.nev,
+        szul: this.szul,
+        email: this.email,
+        nem: this.nem,
+      };
+      console.log(this.nevezes);
+
+      axios.post("http://localhost/backend.php", this.nevezes)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error))
+      
+      // axios.put("http://localhost/backend.php", this.nevezes)
+      // .then(response => console.log(response))
+      
+
+
+      // fetch('http://localhost/backend.php',{
+      //   method: 'POST',
+      //   header: {
+      //     'Content-Type': 'application/json;charset=utf-8'
+      //     },
+      //     body: JSON.stringify(this.nevezes)
+      // })
+      // .then(response => response.json())
+      // .then(result => console.log(result))
     },
-    check(){
-      if (this.nev != ''){
-        this.kitoltve = false
+
+    check() {
+      if (this.nev != "") {
+        this.kitoltve = false;
       }
-    }
+    },
   },
 };
 </script>
